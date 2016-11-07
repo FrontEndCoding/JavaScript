58fa2314225d3c98b80975163181406f19a3e7f5
Maciej MATTHIAS Szczęsny <maciej.szczesny.orzesze@gmail.com>
2016-11-07 05:07:15 +0100

    Add files via upload

---
+++ 1.Podstawy_JavaScript/4.Tablice_wielowymiarowe/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Jacek on 2016-01-11.
+ */
+var task1Array = [
+                  [2,3],
+                  ["ala", "ola"],
+                  [true, false],
+                  [5, 6, 7, 8],
+                  [12, 15, 67]
+              ];
+
+
+var task2Array = [
+                  [1,2,3,4],
+                  [5, 6, 7, 8],
+                  [9, 10, 11 , 12]
+            ];
